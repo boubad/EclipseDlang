eb0bc0a7a54fee43491f64a72763f15c6866ca27
Boubacar Diarra <boubadiarra@gmail.com>
2016-04-07 02:01:04 +0200

    added khideux distance function

--- TestDOne/src/info/data/distance.d
+++ TestDOne/src/info/data/distance.d
@@ -5,7 +5,7 @@ import std.math;
 class DistanceFunc(T=int,Z=long) {
 	this(){
 	}
-	Z opCall(in T[] data1, in T[] data2) const
+	Z opCall(in T[] data1, in T[] data2, in size_t irow1 = 0, in size_t irow2 = 0) const
 	in {
 		assert(!(data1 is null));
 		assert(!(data2 is null));
@@ -15,20 +15,20 @@ class DistanceFunc(T=int,Z=long) {
 		assert(result >= 0);
 	}
 	body{
-		return cast(Z)perform_compute(data1,data2);
+		return cast(Z)perform_compute(data1,data2, irow1, irow2);
 	}// opCall
 	protected:
-		real compute_one_step(in T v1, in T v2, in size_t pos) const 
+		real compute_one_step(in T v1, in T v2, in  size_t icol, in size_t irow1, in size_t irow2) const 
 		{
 			real r = v1 - v2;
 			return (r * r);
 		}// compute_one_step
-		real perform_compute(in T[] data1, in T[] data2) const
+		real perform_compute(in T[] data1, in T[] data2, in size_t irow1, in size_t irow2) const
 		{
 			real s = 0;
 			immutable size_t n = data1.length;
 			for (size_t i = 0; i < n; ++i){
-				s  += compute_one_step(data1[i],data2[i],i);
+				s  += compute_one_step(data1[i],data2[i],i, irow1, irow2);
 			}// i
 			if (n > 1){
 				s /= n;
@@ -44,7 +44,7 @@ class ManhattanDistanceFunc(T=int,Z=long) : DistanceFunc!(T,Z)
         super();
     }
 protected:
-    override real compute_one_step(in T v1, in T v2, in size_t pos) const
+    override real compute_one_step(in T v1, in T v2, in size_t icol, in size_t irow1, in size_t irow2) const
     {
         real r = v1 - v2;
         return abs(r);
@@ -58,12 +58,62 @@ class EuclideDistanceFunc(T=int,Z=long) : DistanceFunc!(T,Z)
         super();
     }
 protected:
-    override real perform_compute(in T[] data1, in T[] data2) const
+    override real perform_compute(in T[] data1, in T[] data2, in size_t irow1, in size_t irow2) const
     {
-        real s = super.perform_compute(data1, data2);
+        real s = super.perform_compute(data1, data2, irow1, irow2);
         return sqrt(s);
     }// perform_compute
 }// class EuclideDistanceFunc
+/////////////////////////////////////////////
+class KhiDeuxDistanceFunc(T=int,Z=double) : DistanceFunc!(T,Z){
+	private:
+		real[] _rowsum;
+		real[] _colsum;
+	public:
+		this(W)(in size_t nRows, in size_t nCols, in W[] data)
+		in {
+			assert(nRows > 0);
+			assert(nCols > 0);
+			assert(!(data is null));
+			assert(data.length >= cast(size_t)(nRows * nCols));
+		}
+		body {
+			_rowsum = [];
+			_colsum = [];
+			_rowsum.length = nRows;
+			_colsum.length = nCols;
+			for (size_t icol = 0; icol < nCols; ++icol){
+				for (size_t irow = 0; irow < nRows; ++irow){
+					_colsum[icol] += data[irow * nCols + icol];
+				}// irow
+				assert(_colsum[icol] > 0);
+			}// icol
+			for (size_t irow = 0; irow < nRows; ++irow){
+				for (size_t icol = 0; icol < nCols; ++icol){
+					_rowsum[irow] += data[irow * nCols + icol];
+				}// irow
+				assert(_rowsum[irow] > 0);
+			}// icol
+		}// body
+		protected:
+ 			 override real compute_one_step(in T v1, in T v2, in size_t icol, in size_t irow1, in size_t irow2) const
+    			{
+    				assert(icol < _colsum.length);
+    				assert(irow1 < _rowsum.length);
+    				assert(irow2 < _rowsum.length);
+        			real r = (v1/_rowsum[irow1]) - (v2/ _rowsum[irow2]);
+        			return ((r * r) / _colsum[icol]);
+    			}// compute_one_step
+			override real perform_compute(in T[] data1, in T[] data2, in size_t irow1, in size_t irow2) const
+				{
+					real s = 0;
+					immutable size_t n = data1.length;
+					for (size_t i = 0; i < n; ++i){
+						s  += compute_one_step(data1[i],data2[i],i, irow1, irow2);
+					}// i
+					return s;
+		}// perform_compute
+}// KhiDeuxDistanceFunc(T=int,Z=long)
 /////////////////////////////////////////////
 class WeightedDistanceFunc(T=int,Z=long) : DistanceFunc!(T,Z)
 {
@@ -91,14 +141,14 @@ public:
         }// i
     }
 protected:
-    override real perform_compute(in T[] data1, in T[] data2) const
+    override real perform_compute(in T[] data1, in T[] data2, in size_t irow1, in size_t irow2) const
     {
         real s = 0;
         immutable size_t n = data1.length;
         assert(weights.length >= n);
         for (size_t i = 0; i < n; ++i)
         {
-            real r = distance_function.compute_one_step(data1[i],data2[i],i);
+            real r = distance_function.compute_one_step(data1[i],data2[i],i, irow1, irow2);
             real c = weights[i];
             assert(c >= 0);
             s  +=  c * r;
